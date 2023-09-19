08f74cc530ef9df981fbb1fac99bc9d38ca1ade9
Beater <104606257+Luis01Felipe@users.noreply.github.com>
2023-09-19 17:29:34 -0300

    Bibliotecas

--- src/bibliotecas/Bibliotecas.java
+++ src/bibliotecas/Bibliotecas.java
@@ -1,8 +1,17 @@
 package bibliotecas;
 // Importando bibliotecas
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
-public class Bibliotecas {
+public class Bibliotecas extends JFrame {
+
+    // Swing
+    JFrame janela = new JFrame();
+    JButton botao = new JButton();
+
+    // AWT
+    Button botao2 = new Button();
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
